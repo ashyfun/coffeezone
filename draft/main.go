ca7d05f93ac0bf8becb0466126fcfb6a7346238b
k1n9pin <k1n9pin@yandex.ru>
2023-07-21 20:38:43 +0300

    Add usage help

--- draft/main.go
+++ draft/main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"os"
 	"sync"
 
 	"github.com/ashyfun/coffeezone"
 )
 
+const UsageOf = `
+Usage of %s: <domain>...
+
+e.g. zoon.ru/msk spb.zoon.ru
+`
+
+func usage() {
+	fmt.Fprintf(os.Stderr, UsageOf, os.Args[0])
+}
+
 func main() {
-	var links = []string{
-		// "zoon.ru/msk",
-		// "spb.zoon.ru",
-		"vladimir.zoon.ru",
+	flag.Usage = usage
+	flag.Parse()
+	if flag.NArg() == 0 {
+		flag.Usage()
 	}
+
+	var links []string = flag.Args()
 	var waitGroup sync.WaitGroup = sync.WaitGroup{}
 	for _, v := range links {
 		v := v
