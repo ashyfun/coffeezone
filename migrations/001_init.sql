2c8b5b4fab40a98b498d4bc671f9d802e73c4e34
k1n9pin <k1n9pin@yandex.ru>
2023-07-31 02:21:25 +0300

    Update init migration

--- migrations/001_init.sql
+++ migrations/001_init.sql
@@ -12,3 +12,15 @@ CREATE TABLE IF NOT EXISTS cz_cafes (
     location_id integer REFERENCES cz_cafe_locations (id) ON DELETE SET NULL,
     updated_at timestamp DEFAULT current_timestamp
 );
+
+CREATE TABLE IF NOT EXISTS cz_topics (
+    id serial PRIMARY KEY,
+    feature varchar(255) NOT NULL,
+    CONSTRAINT feature_unique UNIQUE (feature)
+);
+
+CREATE TABLE IF NOT EXISTS cz_cafes_topics (
+    cafe_code char(24) REFERENCES cz_cafes (code),
+    topic_id integer REFERENCES cz_topics (id),
+    PRIMARY KEY (cafe_code, topic_id)
+);
