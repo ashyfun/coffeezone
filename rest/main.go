e837a88d1b33d730f6dcbba01418dcfcbd8808e2
k1n9pin <k1n9pin@yandex.ru>
2023-08-16 03:27:55 +0300

    Allow CORS for debug mode

--- rest/main.go
+++ rest/main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/ashyfun/coffeezone"
 	"github.com/gin-gonic/gin"
 )
+
+const ACAllow = "Access-Control-Allow"
+
+func cors() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.Writer.Header().Set(ACAllow+"-Origin", "*")
+		ctx.Writer.Header().Set(ACAllow+"-Credentials", "true")
+		ctx.Writer.Header().Set(ACAllow+"-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		ctx.Writer.Header().Set(ACAllow+"-Methods", "POST, OPTIONS, GET, PUT")
+
+		if ctx.Request.Method == "OPTIONS" {
+			ctx.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		ctx.Next()
+	}
+}
 
 func main() {
 	flags := coffeezone.ParseFlags(nil)
@@ -28,6 +47,9 @@ func main() {
 	defer coffeezone.CloseDatabasePool()
 
 	router := NewRouter()
+	if gin.Mode() == "debug" {
+		router.Use(cors())
+	}
 	api1 := router.WrapGroup("/api/v1")
 	{
 		api1.View("GET", "/cafes", CafesHandler)
